7e4462da4c3deef86efa0236b429cff2f01010a4
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-05-18 15:02:20 +0530

    setting up mongoDb

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 let prot =process.env.PORT || 6000 
 
+import connectDb from './config/db.js'
 
 let port =8000
 
@@ -11,9 +12,12 @@ let port =8000
 let app= express()
 
 
-
+app.get("/", (req,res)=>{
+    res.send("hello from server")
+})
 
 
 app.listen(port,()=>{
     console.log("Hello from server")
+    connectDb()
 })
