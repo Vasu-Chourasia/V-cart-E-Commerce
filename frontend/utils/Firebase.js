9cf52a45f79ab383b5d49ca20162a58ac29a0610
Vasu-Chourasia <vdevwork1906@gmail.com>
2026-06-14 15:57:34 +0530

    fixed firebase

--- frontend/utils/Firebase.js
+++ frontend/utils/Firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
     apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-    authDomain: "v-cart-7adf6.firebaseapp.com",
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "v-cart-7adf6.firebaseapp.com",
     projectId: "v-cart-7adf6",
     storageBucket: "v-cart-7adf6.firebasestorage.app",
     messagingSenderId: "938248810096",
